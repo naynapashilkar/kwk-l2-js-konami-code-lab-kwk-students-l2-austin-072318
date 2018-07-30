124454fd8fdce4baca866ce1d417f691dfe79a47
naynapashilkar <nayna.pashilkar@gmail.com>
2018-07-30 14:59:06 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,41 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+// let index = 0
+
+// function init() {
+//   // your code here
+//   const body = document.querySelector("body")
+//     body.addEventListener("keydown", function(e) {
+    
+//     const key = e.key
+    
+//     if (key === codes[index]) {
+//       index++;
+//         if (index===codes.length) {
+//           alert("hurray")
+//         }
+//     }
+// }
+// }
+let index = 0
+
+function init () {
+
+const body = document.querySelector('body')
+
+body.addEventListener('keydown', function(e) {
+  console.log(e.key)
+  const key = e.key
+  if (e.key === codes[index]) {
+    index++;
+      if (index === codes.length) {
+        alert ("hurray")
+        index = 0
+      }
+  }
+  else {
+    index = 0
+  }
+})
 }
